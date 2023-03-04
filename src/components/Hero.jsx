b23ee0e2fd15329ff4737ecdd08eebaad10b98eb
Kyronne <kyronne.parkes@gmail.com>
2023-03-04 23:34:48 +0000

    Added Three JS element and updated hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,9 +14,19 @@ const Hero = () => {
             <div className='w-1 sm:h-80 h-40 violet-gradient'/>
             </div>
             <div>
-              <h1 className={`${styles.heroHeadText} text-white`}>Web3 is the <span className='text-[#915eff] underline underline-offset-2'>Future</span></h1>
+              <h1 className={`${styles.heroHeadText} text-white`}>
+                Web3 is the <span 
+                className='text-[#915eff] underline underline-offset-2'>Future
+                </span></h1>
+                <p className={`{styles.heroSubText} mt-2
+                 text-white-100`}>
+                  Developer of bleeding edge Web3 websites, Web3 <br 
+                  className='sm:block hidden'/> 
+                  dapps crypto & web applications.
+                 </p>
             </div>
         </div>
+        <ComputersCanvas />
     </section>
   )
 }
