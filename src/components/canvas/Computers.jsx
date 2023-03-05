8e06e535b2f2b4727834adfe01abed431b4bac54
Kyronne <kyronne.parkes@gmail.com>
2023-03-05 12:58:18 +0000

    Changed hero for mobile

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,8 +4,8 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+const Computers = ( { isMobile }) => {
+  const computer = useGLTF("./ethereum/scene.gltf");
 
   return (
     <mesh>
@@ -13,8 +13,8 @@ const Computers = () => {
       groundColor='black' />
       <pointLight intensity={1} />
       <spotLight 
-      position={[-20, 50, 10]}
-      angle={0.12}
+      position={[-10, 50, 20]}
+      angle={0.15}
       penumbra={1}
       intensity={1}
       castShadow
@@ -22,20 +22,41 @@ const Computers = () => {
       />
       <primitive 
       object={computer.scene}
-      scale={0.75}
-      position={[0, -3.25, -1.5]}
-      rotation={[-0.01, -0.2, -0.1]}
+      scale={isMobile ? 0.5 : 0.7}
+      position={isMobile ? [0, -1, 0] : [0, 0, 0]}
+      rotation={[-0.01, 1, 0]}
       />
     </mesh>
   );
 };
 
 const ComputersCanvas = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+       const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+      setIsMobile(mediaQuery.matches);
+    
+      const handleMediaQueryChange = (event) => {
+        setIsMobile(event.matches);
+      }
+
+      mediaQuery.addEventListener('change', 
+      handleMediaQueryChange);
+
+      return () => {
+        mediaQuery.removeEventListener('change', 
+        handleMediaQueryChange )
+      }
+
+      }, [])
+    
    return (
     <Canvas
       frameloop='demand'
       shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [20, 1, 20], fov: 22 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
@@ -44,7 +65,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile}/>
       </Suspense>
 
       <Preload all />
