de07294a7cbb272147935853f8232b01f766fd66
Kyronne <kyronne.parkes@gmail.com>
2023-04-02 13:23:56 +0100

    Centered contact info

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div >
+      <a className="w-full flex 
+      items-center py-5 fixed top-0 z-40 bg-primary" href="https://t.me/AgentOfDefi">Contact - Telegram
+      </a>
+      </div>
   )
 }
 
